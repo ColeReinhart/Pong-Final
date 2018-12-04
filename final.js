0ae9c89a4a6112308811e661679e06909448dd3d
Cole Reinhart <colecooler16@hotmail.com>
2018-12-04 13:13:36 -0500

    adding JavaScript and html file to github for first master commit

---
+++ final.js
@@ -0,0 +1,31 @@
+// PONG:
+  // CREATE A BOARD/PLAYING AREA
+  // MAKE A POINT SYSTEM
+  // HAVE A WAY FOR PLAYERS TO MOVE THEIR OBJECT
+  // HAVE A WINNER
+  // ANIMATE THE BALL
+  // MOVE OBJECTS ON COMMAND
+  // RESET THE GAME
+  // HAVE TWO PLAYERS
+
+  /*  1. List all user stories
+      2. Estimate (12358)
+      3. prioritize (order, MVP*,)
+      4. kanban(back-log, in progress, review, done)
+      
+     
+
+
+      /* USER STORIES
+
+      1. as a user i want to be able to see all the objects (paddles, and play square)
+      2. As a user i want to be able to keep score automatically
+      3. As a user i want the game to end after 10 points
+      4. As a user i want to be able to restart my game at anytime
+      5. as a user i want to see who the winner is
+      6. As a user i want the ball to be animated
+      7. as a user i want to be able to move my paddle
+      8. as a user i want to be able to have to players on the same keyboard
+      9. as a user i want a start screen with instructions 
+      */
+    
